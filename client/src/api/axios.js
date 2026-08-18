1c32b8801ee656d041ba5894c7c919dfa67ec0bb
Thanh Thuy <thachthanhthuy287@anhthuy287gmail.uy287gmailcom>
2026-08-18 21:58:46 +0700

    Fix: Remove all remaining localhost endpoints in client

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000', // Đảm bảo đúng port Backend của bạn
+  baseURL: 'https://xeptkbuit.onrender.com/api', // Đảm bảo đúng port Backend của bạn
 });
 
 export default instance;
