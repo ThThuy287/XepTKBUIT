76c4663787fe90fb3e3b8ac1f88fe5af061cf82e
Thanh Thuy <thachthanhthuy287@anhthuy287gmail.uy287gmailcom>
2026-08-19 10:03:57 +0700

    Fix: Remove duplicate /api prefix in fetch courses endpoint

--- client/src/api/courseApi.js
+++ client/src/api/courseApi.js
@@ -1,6 +1,6 @@
 import axios from './axios';
 
 export const getCoursesByImportId = async (importId) => {
-  const response = await axios.get(`/api/courses?importId=${importId}`);
+  const response = await axios.get(`/courses?importId=${importId}`);
   return response.data;
 };
